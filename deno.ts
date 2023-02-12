2d907b14a185f901760da792afaf06e8a12a2f29
Braden Wong <bmw02002turbo@gmail.com>
2023-02-11 23:26:39 -0500

    feat: scaffolding

---
+++ deno.ts
@@ -0,0 +1,30 @@
+// import { promises as fs } from "deno"
+import * as fs from "https://deno.land/std/fs/mod.ts"
+
+const contentDirectory = "/Users/braden/Code/obsidian/"
+const outputDirectory = "/Users/braden/Code/optim/src/content/articles"
+
+const slugify = (fileName: string) =>
+  fileName.replace(/[^a-zA-Z0-9-_]/g, "-").toLowerCase()
+
+const processText = (text: string) =>
+  text.replace(/\[\[([^\]]+)\]\]/g, (_match, fileName) => {
+    const slug = slugify(fileName)
+    const filePath = `${contentDirectory}${fileName}`
+    try {
+      fs.accessSync(filePath)
+      return `[${fileName}](${slug})`
+    } catch {
+      return `*${fileName}*`
+    }
+  })
+
+;(async () => {
+  const files = await fs.readdir(contentDirectory)
+  for (const file of files) {
+    if (!file.endsWith(".md")) continue
+    const text = await fs.readFile(`${contentDirectory}${file}`, "utf-8")
+    const processedText = processText(text)
+    await fs.writeFile(`${outputDirectory}/${file}`, processedText, "utf-8")
+  }
+})().catch((error) => console.error(error))
