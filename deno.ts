f05a2b192e860e653bce5b69c2475ebf059b076a
Braden Wong <bmw02002turbo@gmail.com>
2023-02-12 00:21:34 -0500

    feat: wikilinksToLinks

--- deno.ts
+++ deno.ts
@@ -10,6 +10,31 @@ const slugifyFileName = (fileName: `${string}.md`) =>
     .replace(/[^a-zA-Z0-9-_]/g, "")
     .toLowerCase()
 
+function wikilinksToLinks(stringWithWikilinks: string): string {
+  const wikilinkRegex = /\[\[(.+?)\]\]/g
+  return stringWithWikilinks.replace(wikilinkRegex, (s: string) => {
+    if (!s) return ""
+    // If the wikilink has a pipe, use the text after the pipe as the link text
+    // e.g. [[Some Page|Some Text]] -> [Some Text](some-page)
+    if (s.includes("|")) {
+      const match = s.match(/\[\[(.+?)\|(.+?)\]\]/)
+      if (match) {
+        const page = match[1] as `{string}.md`
+        const text = match[2]
+        return `[${text}](${slugifyFileName(page)})`
+      }
+    }
+
+    // Otherwise, use the page name as the link text
+    const match = s.match(/\[\[(.+?)\]\]/)
+    if (match) {
+      const page = match[1] as `{string}.md`
+      return `[${page}](${slugifyFileName(page)})`
+    }
+    return ""
+  })
+}
+
 const processText = (text: string) =>
   text.replace(/\[\[([^\]]+)\]\]/g, (_match, fileName) => {
     const slug = slugifyFileName(fileName)
