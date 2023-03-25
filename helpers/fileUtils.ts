263a165ab389e7b13325c170e6ff6b42559e03c2
Braden Wong <bmw02002turbo@gmail.com>
2023-03-25 11:10:42 -0400

    feat: getMarkdownFilesData

--- helpers/fileUtils.ts
+++ helpers/fileUtils.ts
@@ -1,14 +1,14 @@
 import { isCriteriaMet } from "./isCriteriaMet.ts"
 import { slugifyFileName } from "./slugifyFileName.ts"
 
-export async function getMarkdownFiles(
+export async function getMarkdownFileSlugs(
   directoryPath: string
 ): Promise<Set<string>> {
   const markdownFiles = new Set<string>()
   for await (const dirEntry of Deno.readDir(directoryPath)) {
     const path = `${directoryPath}/${dirEntry.name}`
     if (dirEntry.isDirectory) {
-      const subDirectoryMarkdownFiles = await getMarkdownFiles(path)
+      const subDirectoryMarkdownFiles = await getMarkdownFileSlugs(path)
       subDirectoryMarkdownFiles.forEach((markdownFile) => {
         markdownFiles.add(`${slugifyFileName(markdownFile)}`)
       })
@@ -40,6 +40,29 @@ export async function getMarkdownFilePaths(
       markdownFiles.add(filePath)
     }
   }
+  return markdownFiles
+}
+
+export async function getMarkdownFilesData(
+  directoryPath: string
+): Promise<Map<string, { filePath: string; content: string }>> {
+  const markdownFiles = new Map<string, { filePath: string; content: string }>()
+
+  async function traverseDirectory(path: string) {
+    for await (const dirEntry of Deno.readDir(path)) {
+      const entryPath = `${path}/${dirEntry.name}`
+      if (dirEntry.isDirectory) {
+        await traverseDirectory(entryPath)
+      } else if (entryPath.endsWith(".md")) {
+        const filePath = `${path}/${dirEntry.name}` as `${string}.md`
+        const fileText = await Deno.readTextFile(filePath)
+        if (!isCriteriaMet({ filePath, fileText })) continue
+        const slug = slugifyFileName(dirEntry.name.slice(0, -3))
+        markdownFiles.set(slug, { filePath, content: fileText })
+      }
+    }
+  }
+  await traverseDirectory(directoryPath)
   return markdownFiles
 }
 
