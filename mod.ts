32e28c25dd32d555e6b70755ed658ccb8cae1eff
Braden Wong <bmw02002turbo@gmail.com>
2023-03-26 12:38:35 -0400

    fix: lowercase obsidian

--- mod.ts
+++ mod.ts
@@ -2,10 +2,10 @@ import { copyDirectory } from "./actions/copyDirectory.ts"
 import { obsidianExport } from "./actions/obsidianExport.ts"
 
 export const contentDirectory =
-  "/Users/braden/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian"
+  "/Users/braden/Library/Mobile Documents/iCloud~md~obsidian/Documents/obsidian"
 const contentOutputDirectory = "/Users/braden/Code/optim/src/content/articles"
 const assetsDirectory =
-  "/Users/braden/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian/assets"
+  "/Users/braden/Library/Mobile Documents/iCloud~md~obsidian/Documents/obsidian/assets"
 const assetsOutputDirectory = "/Users/braden/Code/optim/public/assets"
 
 await obsidianExport(contentDirectory, contentOutputDirectory)
