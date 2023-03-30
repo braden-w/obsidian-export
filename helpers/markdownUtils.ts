4d3ad49164a61c1433822c7ebf60f7d11cb02e56
Braden Wong <bmw02002turbo@gmail.com>
2023-03-29 21:51:02 -0400

    feat: zod schema

--- helpers/markdownUtils.ts
+++ helpers/markdownUtils.ts
@@ -1,4 +1,23 @@
 import { MarkdownFileSummary, Slug } from "../types.d.ts"
+import { z } from "https://deno.land/x/zod/mod.ts"
+
+export const articleSchema = z.object({
+  title: z.string().optional(),
+  alias: z.string().nullable(),
+  tags: z
+    .string()
+    .nullable()
+    .transform((tags) => tags?.split(",").map((tag) => tag.trim())),
+  resonance: z.number().nullable(),
+  subtitle: z.string().nullable(),
+  date: z.date(),
+  "date modified": z.date().nullable(),
+  "link-twitter": z.string().url().nullable(),
+  "link-medium": z.string().url().nullable(),
+  "link-substack": z.string().url().nullable(),
+  "link-reddit": z.string().url().nullable(),
+  "link-github": z.string().url().nullable(),
+})
 
 export async function getMarkdownFileSummary(
   entryPath: `${string}/${string}.md`,
