2b49a2a7fcc69aa9fac9288ca0a0c4d875646be7
Braden Wong <bmw02002turbo@gmail.com>
2023-05-25 23:25:12 -0700

    refactor: frontmatter

---
+++ helpers/markdown/frontmatter.ts
@@ -0,0 +1,35 @@
+import { parse } from "https://deno.land/x/frontmatter/mod.ts"
+import { z } from "https://deno.land/x/zod/mod.ts"
+
+import { MarkdownFileSummary } from "../../types.d.ts"
+
+export const articleSchema = z.object({
+  title: z.string().optional(),
+  alias: z.string().nullable(),
+  tags: z
+    .string()
+    .nullable()
+    .transform((tags) => tags?.split(",").map((tag) => tag.trim())),
+  resonance: z.number().nullable(),
+  subtitle: z.string().nullable(),
+  date: z.date(),
+  "date modified": z.date().nullable(),
+  "link-twitter": z.string().url().nullable(),
+  "link-medium": z.string().url().nullable(),
+  "link-substack": z.string().url().nullable(),
+  "link-reddit": z.string().url().nullable(),
+  "link-github": z.string().url().nullable(),
+})
+
+export function getArticleFrontmatter({
+  fileText,
+  fileName,
+}: MarkdownFileSummary) {
+  try {
+    const { data } = parse(fileText)
+    const dataParsed = articleSchema.parse(data)
+    return { data: dataParsed }
+  } catch (error) {
+    return { error: { fileName, message: error.message } }
+  }
+}
