0ea2d2717fcc779c206bc9eb83d51ed427d0084d
Braden Wong <bmw02002turbo@gmail.com>
2023-05-26 13:36:28 -0700

    chore: output directory

--- mod.ts
+++ mod.ts
@@ -2,10 +2,10 @@ import { obsidianExport, copyDirectory } from "./actions/obsidianExport.ts"
 
 export const contentDirectory =
   "/Users/braden/Library/Mobile Documents/iCloud~md~obsidian/Documents/obsidian"
-const contentOutputDirectory = "/Users/braden/Code/optim/src/content/articles"
+const contentOutputDirectory = "/Users/braden/Code/optim/apps/blog-sveltekit/src/content/articles"
 const assetsDirectory =
   "/Users/braden/Library/Mobile Documents/iCloud~md~obsidian/Documents/obsidian/assets"
-const assetsOutputDirectory = "/Users/braden/Code/optim/public/assets"
+const assetsOutputDirectory = "/Users/braden/Code/optim/apps/blog-sveltekit/public/assets"
 
 await obsidianExport(contentDirectory, contentOutputDirectory)
 await copyDirectory(assetsDirectory, assetsOutputDirectory)
